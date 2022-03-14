895fac210c90233a00f7aa171d0d868ad416e226
Renato de Oliveira <41616034874@admin.br>
2022-03-14 10:34:18 -0300

    Trabalhando com o methodo GET em PHP

--- email.php
+++ email.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Funcionando";
+$name = $_POST['name'];
+$lastName = $_POST['lastName'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+echo "<p>$name, Seja-bem vindo! <br>Informamos que os dados foram armazenados com sucesso!</p>";
+
+echo "Nome: $firstName <br>";
+echo "Sobrenome: $lastName <br>";
+echo "Idade: $age <br>";
+echo "Email: $email <br>";
+echo "Mensagem  : $message <br>";
